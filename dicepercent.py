97a9b018fffeeadcd164b5bb7423089b1044f2f8
David Annable <david.annable@gmail.com>
2023-02-07 12:17:50 -0500

    Initial commit Changes to be committed:     new file:   dicepercent.py

---
+++ dicepercent.py
@@ -0,0 +1,50 @@
+import random
+
+def rolld100():
+    
+    result = 0
+    bonusDice = 0
+    penaltyDice = 0
+    tens_die_rolls = []
+
+    bonusDice = int(input('Any bonus dice?[0]') or "0")
+    penaltyDice = int(input('Any penalty dice?[0]') or "0")
+    resultDice = abs(bonusDice - penaltyDice) + 1
+
+
+    if bonusDice > penaltyDice:
+        # Determine and print the number of Bonus dice to roll
+        print('Rolling d100 with ' + str(resultDice -1) + ' bonus dice.\n')
+
+        for i in range(resultDice):
+            tens_die_roll = random.randint(0,9)
+            print('Tens die ' + str(i+1) + ': ' + str(tens_die_roll))
+            tens_die_rolls.append(tens_die_roll)
+            d1 = max(tens_die_rolls)
+
+    elif penaltyDice > bonusDice:
+        # Determine and print the number of Penalty dice to roll
+        print('Rolling d100 with ' + str(resultDice -1) + ' penalty dice.\n')
+  
+        for i in range(resultDice):
+            tens_die_roll = random.randint(0,9)
+            print('Tens die ' + str(i+1) + ': ' + str(tens_die_roll))
+            tens_die_rolls.append(tens_die_roll)
+            d1 = min(tens_die_rolls)
+
+    else:
+        # Determine and print the number of dice to roll
+        print('Rolling d100 with no bonus or penalty dice.\n')
+        d1 = random.randint(0,9)
+        print('Tens die: ' + str(d1))
+
+    d2 = random.randint(0,9)
+    print ("Ones die: " + str(d2))
+
+    if d1 == 0 and d2 == 0:
+        result = 100
+    else:
+        result = d1 * 10 + d2
+    return result
+
+print("Final roll: " + str(rolld100()))
