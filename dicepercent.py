b4d4166de27213e55f5a797e378f67f78878ab63
David Annable <david.annable@gmail.com>
2023-02-13 10:23:42 -0500

    Fixed issue where rolls of 100 were not being evaluated properly.

--- dicepercent.py
+++ dicepercent.py
@@ -11,6 +11,8 @@ def rolld100():
     penaltyDice = int(input('Any penalty dice?[0]') or "0")
     resultDice = abs(bonusDice - penaltyDice) + 1
 
+    d2 = random.randint(0,9)
+    
 
     if bonusDice > penaltyDice:
         # Determine and print the number of Bonus dice to roll
@@ -18,6 +20,8 @@ def rolld100():
 
         for i in range(resultDice):
             tens_die_roll = random.randint(0,9)
+            if d2 == 0 and tens_die_roll == 0:
+                tens_die_roll =100 
             print('Tens die ' + str(i+1) + ': ' + str(tens_die_roll))
             tens_die_rolls.append(tens_die_roll)
             d1 = max(tens_die_rolls)
@@ -38,10 +42,9 @@ def rolld100():
         d1 = random.randint(0,9)
         print('Tens die: ' + str(d1))
 
-    d2 = random.randint(0,9)
     print ("Ones die: " + str(d2))
 
-    if d1 == 0 and d2 == 0:
+    if d1 == 100 and d2 == 0:
         result = 100
     else:
         result = d1 * 10 + d2
